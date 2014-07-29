54f590addcbd1240b3ab376187ab4046d78ed9e2
Nakane Kazuya <nakane.kazuya@isid.co.jp>
2014-07-29 10:10:55 +0900

    user名の入力を追加

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ type Message struct {
 
 type Page struct {
 	Thread   string
+	Username string
 	Messages []Message
 }
 
@@ -20,18 +21,36 @@ var pages = make(map[string][]Message)
 
 func main() {
 	pages["Test"] = []Message{{User: "system", Text: "Hi, write your message"}}
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/login", loginHandler)
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/send/", sendHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	temp, _ := template.ParseFiles("index.html")
+	temp.Execute(w, nil)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	cookie := http.Cookie{Name: "username", Value: username}
+	http.SetCookie(w, &cookie)
+	http.Redirect(w, r, "/view/Test", http.StatusFound)
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	thread := r.URL.Path[len("/view/"):]
+	username, _ := r.Cookie("username")
+	if username.Value == "" {
+		http.Redirect(w, r, "/", http.StatusBadRequest)
+	}
 	if pages[thread] == nil {
 		log.Println("nil")
 	}
-	p := &Page{Thread: thread, Messages: pages[thread]}
-	temp, _ := template.ParseFiles("index.html")
+	p := &Page{Thread: thread, Username: username.Value, Messages: pages[thread]}
+	temp, _ := template.ParseFiles("room.html")
 	err := temp.Execute(w, p)
 	if err != nil {
 		log.Println(err)
@@ -43,6 +62,8 @@ func sendHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("send")
 	thread := r.URL.Path[len("/send/"):]
 	message := r.FormValue("message")
-	pages[thread] = append(pages[thread], Message{User: "user1", Text: message})
-	http.Redirect(w, r, "/view/"+thread, http.StatusFound)
+	username, _ := r.Cookie("username")
+	pages[thread] = append(pages[thread], Message{User: username.Value, Text: message})
+	viewHandler(w, r)
+
 }
